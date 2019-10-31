aa1e39c1b4b98cdbad340dc57802389134ba2edd
Matheus Araujo <mathqah@gmail.com>
2019-10-31 17:38:50 -0300

    [ADD] Basic operations

---
+++ Calculator/Calculator/Calculator.swift
@@ -0,0 +1,24 @@
+//
+//  Calculator.swift
+//  Calculator
+//
+//  Created by Matheus Hoppy on 31/10/19.
+//  Copyright © 2019 Lucas Tavares. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorOperations{
+    func plus(a: Int, b: Int) -> Int{
+        return a + b
+    }
+    func minus(a: Int, b: Int) -> Int{
+        return a - b
+    }
+    func multiply(a: Int, b: Int) -> Int{
+        return a * b
+    }
+    func divide(a: Int, b: Int) -> Int{
+        return a / b
+    }
+}
