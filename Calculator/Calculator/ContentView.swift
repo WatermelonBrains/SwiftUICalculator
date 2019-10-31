df15f885c02c8d65a4f219291b1d2c4e00f1580f
tavares <lucastavaresvasconcelos@gmail.com>
2019-10-31 09:58:37 -0300

    Initial configuration of the project. Let's use some cool things like MVVM and ViewCode 😜

---
+++ Calculator/Calculator/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Lucas Tavares on 31/10/19.
+//  Copyright © 2019 Lucas Tavares. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ContentView: UIView {
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		backgroundColor = .red
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+}
---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Lucas Tavares on 31/10/19.
+//  Copyright © 2019 Lucas Tavares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	
+	let contentView = ContentView()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view = contentView
+		
+	}
+	
+}
+
