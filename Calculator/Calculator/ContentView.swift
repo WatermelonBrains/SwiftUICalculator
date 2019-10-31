53b949099636c90ef3971bf172aca7903122330c
tavares <lucastavaresvasconcelos@gmail.com>
2019-10-31 11:13:30 -0300

    [ADD] Add display of calculator

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -9,15 +9,41 @@
 import Foundation
 import UIKit
 
-class ContentView: UIView {
-
+class ContentView: UIView, ConfigurableView {
+	
+	
+	let displayLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.backgroundColor = .black
+		label.text = "19,8"
+		label.textColor = .white
+		label.textAlignment = .right
+ 		label.font = label.font.withSize(64)
+		return label
+	}()
+	
 	override init(frame: CGRect) {
 		super.init(frame: frame)
-		backgroundColor = .red
+		backgroundColor = .black
+		setupView()
 	}
 	
 	required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
+	}
+	
+	func buildHierarchy() {
+		addSubview(displayLabel)
+	}
+	
+	func addConstraints() {
+		NSLayoutConstraint.activate([
+			displayLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 32),
+			displayLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+			displayLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+//			displayLabel.heightAnchor.constraint(equalToConstant: 200)
+		])
 	}
 	
 }
